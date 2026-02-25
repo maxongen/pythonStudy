31f24a6d87e113159c06036b7ab577a7fd490db3
maxongen <maxongen41@gmail.com>
2026-02-25 21:54:24 +0530

    adding code

--- src/main/calculatorProject/calculator_if_else.py
+++ src/main/calculatorProject/calculator_if_else.py
@@ -1,3 +1,24 @@
-operationVal=input("Enter Operation to Perform : ")
-number1=input("Enter First Number : ")
-number2=input("Enter Second Number : ")
+operationVal=input("Please select any option number(e.g. 1, 2, 3, 4) : ")
+number1=float(input("Enter First Number : "))
+number2=float(input("Enter Second Number : "))
+
+
+if operationVal=="1":
+  print("Addition of ",number1," and ",number2)
+  number3=number1+number2
+  print("Addition : ",number3)
+elif operationVal=="2":
+  print("Substraction of ",number1," and ",number2)
+  number3=number1-number2
+  print("Substraction : ",number3)
+elif operationVal=="3":
+  print("Multiplication of ",number1," and ",number2)
+  number3=number1*number2
+  print("Multiplication : ",number3)
+elif operationVal=="4":
+  print("Division of ",number1," and ",number2)
+  number3=number1/number2
+  print("Division : ",number3)
+else:
+  print("provided wrong operation type")
+
